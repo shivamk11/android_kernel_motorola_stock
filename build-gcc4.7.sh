0b1de19c837b347fee7e9f269bba1dfd94c700b1
shivamk11 <shivamk11@gmail.com>
2014-08-15 10:56:06 +0530

    Add build scripts Signed-off-by: shivamk11 <shivamk11@gmail.com>

---
+++ build-gcc4.7.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+TOOLCHAIN="/home/shivam/development/arm-linux-androideabi-4.7/bin/arm-linux-androideabi"
+MODULES_DIR="/home/shivam/development/modules"
+ZIMAGE="/home/shivam/development/android_kernel_mototola_msm8610/arch/arm/boot/zImage-dtb"
+KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8610"
+BUILD_START=$(date +"%s")
+if [ -a $ZIMAGE ];
+then
+rm $ZIMAGE
+rm $MODULES_DIR/*
+fi
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- phantom_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8 CONFIG_NO_ERROR_ON_MISMATCH=y
+if [ -a $ZIMAGE ];
+then
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$TOOLCHAIN-strip --strip-unneeded *.ko
+cd $KERNEL_DIR
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+else
+echo "Compilation failed! Fix the errors!"
+fi
