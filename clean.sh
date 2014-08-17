2a93e3016c567dc0ee39266bf690b7958ddeaffe
shivamk11 <shivamk11@gmail.com>
2014-08-17 19:11:55 +0530

    clean.sh:remove dt.img too

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
-KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8610"
+KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8226_stock"
 make ARCH=arm CROSS_COMPILE=/home/shivam/development/toolchains/linaro-4.8-generic/bin/arm-gnueabi- -j8 clean mrproper
 rm -rf $KERNEL_DIR/ramdisk.cpio
 rm -rf $KERNEL_DIR/root.fs
 rm -rf $KERNEL_DIR/boot.img
+rm -rf $KERNEL_DIR/dt.img
