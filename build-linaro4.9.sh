4d4fcd89739a262db904eb7af398bde54de2c98b
shivamk11 <shivamk11@gmail.com>
2014-09-09 17:26:55 +0530

    A few changes in build script

--- build-linaro4.9.sh
+++ build-linaro4.9.sh
@@ -6,6 +6,7 @@ KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8226_stock"
 MKBOOTIMG="/home/shivam/boot-tools-falcon/tools/mkbootimg"
 MKBOOTFS="/home/shivam/boot-tools-falcon/tools/mkbootfs"
 DTBTOOL="/home/shivam/boot-tools-falcon/tools/dtbTool"
+DTBTOOLCM="/home/shivam/boot-tools-falcon/tools/dtbToolCM"
 BUILD_START=$(date +"%s")
 if [ -a $ZIMAGE ];
 then
@@ -26,7 +27,7 @@ cd $KERNEL_DIR
 $DTBTOOL -o dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/
 $MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
 cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
-$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --base 0x00000000 --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags" --pagesize 2048 --dt dt.img -o $KERNEL_DIR/boot.img
+$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --pagesize 2048 --base 0x10000000 --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags' --dt dt.img -o $KERNEL_DIR/boot.img
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
